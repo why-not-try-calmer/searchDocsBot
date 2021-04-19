54d7eb38e932489288fb65331fd9e9da7ab0ed1d
Nycticorax <mrnycticorax@gmail.com>
2021-04-19 13:06:37 +0200

    ---
    Signed-off-by: Nycticorax <mrnycticorax@gmail.com>

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 const Slimbot = require('slimbot');
-const slimbot = new Slimbot(process.env['TELEGRAM_TOKEN']);
-// const restify = require('restify');
-// const restifyBodyParser = require('restify-plugins').bodyParser;
+const slimbot = new Slimbot(process.env['TELEGRAM_TOKEN'] || '1746181670:AAGGB9trk_ro8lRlLztjtI5Mw8DiptcnqFs');
+const restify = require('restify');
 
-const search_handle = require ('./lib.js')
- 
-// Register listeners
- 
-slimbot.on('message', message => {
-  slimbot.sendMessage(message.chat.id, 'Message received');
-});
- 
+let server = restify.createServer();
+
+server.use(restify.plugins.bodyParser());
+
 // Setup webhook integration
-slimbot.setWebhook('https://www.example.com');
- 
+slimbot.setWebhook({ url: process.env['BASE_URL'] || 'https://opensuse-docs-bot.herokuapp.com/'});
+
 // Get webhook status
 slimbot.getWebhookInfo();
- 
-// Teardown webhook integration
-slimbot.deleteWebhook();
+
+// Handle updates (example)
+server.post('/bot_updates', function handle(req, res) {
+  let update = req.body;
+  // handle type of update here...
+  // i.e. if (update.message) { ... }
+});
+
+server.listen(8443);
