64158dd695a6c2f9e4304e1e957d73a0ee22d61e
Nycticorax <mrnycticorax@gmail.com>
2021-04-19 16:18:42 +0200

    ---
    Signed-off-by: Nycticorax <mrnycticorax@gmail.com>

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let server = restify.createServer();
 server.use(restify.plugins.bodyParser());
 
 const parsed = s => {
-    const mention = '@opensuse_dr_docs'
+    const mention = '@opensuse_docs_bot'
     const command = '/docs'
     const head = s.slice(0, 5)
     if (head === command) return s.slice(6)
