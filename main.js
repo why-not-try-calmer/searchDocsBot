63935f7d3bf0c3eacd20f2cbe03d876fbba95f24
Nycticorax <mrnycticorax@gmail.com>
2021-04-20 13:13:16 +0200

    ---
    Signed-off-by: Nycticorax <mrnycticorax@gmail.com>

--- main.js
+++ main.js
@@ -44,10 +44,10 @@ const bot_handle = (req, res, next) => {
     }
     const found_in_parse = parsed(message_text)
     if (found_in_parse !== null) {
-        return search_handle(found_in_parse).then(res => {
+        return search_handle(found_in_parse).then(found => {
             const user = username === undefined ? '' : '@' + username + '\n'
-            const text = res !== null
-                ? user + res
+            const text = found !== null
+                ? user + found
                 : 'No result about this yet, but keep tabs on ' + DOCS_URL + ' in the upcoming days'
             const optParams = { reply_to_message_id: parseInt(message_id) }
             slimbot.sendMessage(chat_id, text, optParams)
