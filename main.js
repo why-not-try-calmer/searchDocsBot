5ee925e977716f9de5c7cd52995cd700504e587c
why-not-try-calmer <mrnycticorax@gmail.com>
2021-06-07 15:58:12 +0200

    ---
    Signed-off-by: why-not-try-calmer <mrnycticorax@gmail.com>

--- main.js
+++ main.js
@@ -180,7 +180,7 @@ server.get('/wakeup', (_, res, next) => {
 })
 
 server.get('/search/:keywords', (req, res, next) => {
-    print (req.params)
+    console.log(req.params)
     const parsed = parse(req.params.keywords)
     if (parsed.Err) {
         res.json("Couldn't parse your input: " + parsed.Err)
